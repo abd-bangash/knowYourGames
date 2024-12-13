e0f9c3dc4c39592c70ee7a8ae86feb57c35c387b
abd-bangash <khanabdulahadmuhammad@gmail.com>
2024-12-13 19:26:18 +0500

    platform icons in game card

--- src/componets/GameCard.jsx
+++ src/componets/GameCard.jsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { PlatformIconsList } from "./PlatformIconsList";
 
 export const GameCard = ({ game }) => {
   return (
@@ -7,6 +8,9 @@ export const GameCard = ({ game }) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconsList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
