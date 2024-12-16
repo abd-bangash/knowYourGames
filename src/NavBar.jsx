bb55c4ddc878f7738604364dfe228d3984535c65
abd-bangash <khanabdulahadmuhammad@gmail.com>
2024-12-16 18:35:55 +0500

    implement searching

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,11 +3,11 @@ import logo from "../assets/Logo/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { SearchInput } from "./componets/SearchInput";
 
-function NavBar() {
+function NavBar({ onSearch }) {
   return (
     <HStack padding={"10px"}>
       <Image src={logo} boxSize={"60px"}></Image>
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/componets/SearchInput.jsx
+++ src/componets/SearchInput.jsx
@@ -1,15 +1,26 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-export const SearchInput = () => {
+export const SearchInput = ({ onSearch }) => {
+  const ref = useRef(null);
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        placeholder="Search games..."
-        variant={"filled"}
-        borderRadius={20}
-      />
-    </InputGroup>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          placeholder="Search games..."
+          ref={ref}
+          variant={"filled"}
+          borderRadius={20}
+        />
+      </InputGroup>
+    </form>
   );
 };
