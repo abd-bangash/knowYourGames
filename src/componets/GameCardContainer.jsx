317d1a3b28325858a37b1b91d4b114a6c93a425f
abd-bangash <khanabdulahadmuhammad@gmail.com>
2024-12-16 19:43:36 +0500

    cutomize them to get darker greys

--- src/componets/GameCardContainer.jsx
+++ src/componets/GameCardContainer.jsx
@@ -1,6 +1,4 @@
 import { Box } from "@chakra-ui/react";
-import { Children } from "react";
-
 export const GameCardContainer = ({ children }) => {
   return (
     <Box borderRadius={"10px"} overflow={"hidden"}>
